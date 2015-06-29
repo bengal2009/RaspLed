6bf50f235b37ebe74cefd516c3c941330e19f702
bengal2009 <bennylin@outlook.com>
2015-06-29 17:48:37 +0800

    V1

--- src/BaiduWeather.java
+++ src/BaiduWeather.java
@@ -3,6 +3,7 @@
  */
 
 import net.sf.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -12,7 +13,7 @@ import java.net.URL;
 import java.net.URLConnection;
 
 
-public class Weather {
+public class BaiduWeather {
     String Ctiyid;
     URLConnection connectionData;
     StringBuilder sb;
@@ -65,12 +66,12 @@ public class Weather {
 
 
 
-    public Weather(String Cityid) throws IOException ,NullPointerException{
+    public BaiduWeather(String Cityid) throws IOException ,NullPointerException{
         // �ѪR����ip�a�}
 
         this.Ctiyid = Cityid;
         // ?�������a�H�x��API
-        URL url = new URL("http://m.weather.com.cn/atad/" + Ctiyid + ".html");
+        URL url = new URL("http://api.map.baidu.com/telematics/v3/weather?location=%E4%B8%8A%E6%B5%B7&output=json&ak=Ib0QNBwBbmsdYRq2EHMI18jp");
         connectionData = url.openConnection();
         connectionData.setConnectTimeout(1000);
         try {
@@ -87,53 +88,14 @@ public class Weather {
         }
         String datas = sb.toString();
         jsonData = JSONObject.fromObject(datas);
-        info = jsonData.getJSONObject("weatherinfo");
+        info = jsonData.getJSONObject("results");
+        city = info.getString("currentCity").toString();
 
-        city = info.getString("city").toString();
-        week =  info.getString("week").toString();
-        date_y = info.getString("date_y").toString();
-        fchh = info.getString("fchh").toString();
-        //1��6�Ѫ����a
-        weather1 =  info.getString("weather1").toString();
-        weather2 =  info.getString("weather2").toString();
-        weather3 =  info.getString("weather3").toString();
-        weather4 =  info.getString("weather4").toString();
-        weather5 =  info.getString("weather5").toString();
-        weather6 =  info.getString("weather6").toString();
-        //1��6�Ѫ��a?
-        temp1 = info.getString("temp1").toString();
-        temp2 = info.getString("temp2").toString();
-        temp3 = info.getString("temp3").toString();
-        temp4 = info.getString("temp4").toString();
-        temp5 = info.getString("temp5").toString();
-        temp6 = info.getString("temp6").toString();
-        //1��6�Ѫ�??
-        wind1 = info.getString("wind1").toString();
-        wind2 = info.getString("wind2").toString();
-        wind3 = info.getString("wind3").toString();
-        wind4 = info.getString("wind4").toString();
-        wind5 = info.getString("wind5").toString();
-        wind6 = info.getString("wind6").toString();
-        //1��6�Ѫ�?�t
-        fl1 = info.getString("fl1").toString();
-        fl2 = info.getString("fl2").toString();
-        fl3 = info.getString("fl3").toString();
-        fl4 = info.getString("fl4").toString();
-        fl5 = info.getString("fl5").toString();
-        fl6 = info.getString("fl6").toString();
-        //�U�����a��?
-        index = info.getString("index").toString();
-        index_uv = info.getString("index_uv").toString();
-        index_tr = info.getString("index_tr").toString();
-        index_co= info.getString("index_co").toString();
-        index_cl = info.getString("index_cl").toString();
-        index_xc = info.getString("index_xc").toString();
-        index_d =  info.getString("index_d").toString();
         System.out.println(city);
     }
     public static void main(String[] args) {
         try {
-            new Weather("101020100"); //  101270803�N�O�A�������N?
+            new BaiduWeather("101020100"); //  101270803�N�O�A�������N?
         } catch (NullPointerException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
